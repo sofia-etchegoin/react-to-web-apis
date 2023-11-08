337746146b23a1fed015e9cd6eef6282a2f81c71
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-11-09 12:00:26 +1300

    Initial commit, added useEffect and useState to App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react'
+import { Widget } from '../../models/Widget'
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Widget[])
+  useEffect(() => {
+    console.log('using the effect')
+  })
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
